3826ae340bdb11d99c4fd4304a7b1a7db7df3d87
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-03-28 13:57:22 +0300

    laba1_DmitrievaYulia

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,64 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str) {
+    int  N = 0;
+    bool let = false, num = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            let = true;
+        }
+        if (isdigit(str[i])) {
+            num = true;
+        }
+        if (isspace(str[i]) && let) {
+            if (let && !num) {
+                N++;
+            }
+            let = false;
+            num = false;
+        }
+    }
+    if (let && !num) {
+        N++;
+    }
+    return N;
 }
-
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+    int N = 0;
+    bool first = true, check = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (isupper(str[i]) && first) {
+            first = false;
+        }
+        if (!isalpha(str[i]) && !isspace(str[i]))check = true;
+        if (isspace(str[i])) {
+            if (!check && !first)N++;
+            first = true;
+            check = false;
+        }
+    }
+    if (!check && !first)N++;
+    return N;
 }
-
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+    int Nwords = 0, Nlet = 0;
+    bool word = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            Nlet++;
+            word = true;
+        }
+        if (isspace(str[i]) && word) {
+            Nwords++;
+            word = false;
+        }
+    }
+    if (word)Nwords++;
+    float lenght = static_cast<double>(Nlet) / Nwords;
+    return round(lenght);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+
 #include "fun.h"
 
+
+
 int main() {
+    const char *str = "qwe4  gftt e 555gr rrgr grhh Lat Nat";
+    std::cout << faStr1(str) << std::endl;
+    std::cout << faStr2(str) << std::endl;
+    std::cout << faStr3(str) << std::endl;
     return 0;
 }
