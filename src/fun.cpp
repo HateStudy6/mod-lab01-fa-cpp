f1a5b53a820cfbfced5384a78a110e6990b4db5f
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 22:20:14 +0300

    dmitrieva

--- src/fun.cpp
+++ src/fun.cpp
@@ -8,11 +8,11 @@ unsigned int faStr1(const char* str) {
   int sl1 = 0; int sl2 = 0; int stroka = 0;
   for ( int i = 0; strlen(str) > i; i++ )
   { if (isalpha(str[i]))
-  {sl1 + = 1;}
+  {sl1 +=  1;}
   if (isdigit(str[i]))
-  {sl2 + = 1;}
+  {sl2 +=  1;}
   if (isspace(str[i]))
-  { if ((sl1 > 0) && (sl2 = = 0))
+  { if ((sl1 > 0) && (sl2  ==  0))
   {stroka++;}
   sl1 = 0;
   sl2 = 0;
