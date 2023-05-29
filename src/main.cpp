7cb41de641cd765561c13fba93fb288d785284f0
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 22:33:40 +0300

    DmitrievaY

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 
 
 
-int main() 
+int main()
 {
     const char *str = "                     ";
     std::cout << faStr1(str) << std::endl;
-    str= "Hello;)";
+    str = "Hello;)";
     std::cout << faStr2(str) << std::endl;
-    str= "Hello;)";
+    str = "Hello;)";
     std::cout << faStr3(str) << std::endl;
-    str= "Hello;)";
+    str = "Hello;)";
     return 0;
 }
