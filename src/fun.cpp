1523a87983abf2ab7c297877b336b315d448476f
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 21:02:30 +0300

    New

--- src/fun.cpp
+++ src/fun.cpp
@@ -9,17 +9,17 @@
 unsigned int faStr1(const char* str)
 
  {
-     int sl1=0; int sl2=0; int stroka=0;
-     for (int i = 0; strlen(str)>i; i++)
+     int sl1 = 0; int sl2 = 0; int stroka = 0;
+     for (int i = 0; strlen(str) > i; i++)
      { if (isalpha(str[i]))
-      {sl1+=1;}
+      {sl1 += 1;}
      if (isdigit(str[i]))
-      {sl2+=1;}
+      {sl2 += 1;}
      if (isspace(str[i]))
-      {if ((sl1>0)&&(sl2 == 0))
+      {if ((sl1 > 0)&&(sl2 == 0))
       {stroka++;}
-      sl1=0;
-      sl2=0;
+      sl1 = 0;
+      sl2 = 0;
      } 
      }
  return stroka;
@@ -30,17 +30,17 @@ unsigned int faStr1(const char* str)
 unsigned int faStr2(const char* str)
 
  {
- int sl1=0; int sl2=0; int stroka=0;
-    for (int i = 0; strlen(str)>i; i++ )
+ int sl1 = 0; int sl2 = 0; int stroka = 0;
+    for (int i = 0; strlen(str) > i; i++ )
     { if (isdigit(str[i]) || ispunct(str[i]))
-    { sl2+=1; }
-    if (isupper(str[i]) && (sl1=0))
-    { sl1=1;}
+    { sl2 += 1; }
+    if (isupper(str[i]) && (sl1 = 0))
+    { sl1 = 1;}
     if (isspace(str[i]))
-    {if ((sl1=1) && (sl2 == 0))
+    {if ((sl1 = 1) && (sl2 == 0))
     {stroka++;}
-    sl1=0;
-    sl2=0;
+    sl1 = 0;
+    sl2 = 0;
     }
     }
  return stroka;
@@ -50,17 +50,17 @@ unsigned int faStr2(const char* str)
 unsigned int faStr3(const char* str)
 
 {
-    int sl1=0; int sl2=0; int stroka=0;
-    float SrDlina=0;
-    for (int i = 0; strlen(str)>i; i++)
+    int sl1 = 0; int sl2 = 0; int stroka = 0;
+    float SrDlina = 0;
+    for (int i = 0; strlen(str) > i; i++)
      {
         if (isalpha(str[i]))
         {
          sl2++;
-         stroka=1;
+         stroka = 1;
         }
         if (isspace(str[i]) && (stroka == 1))
-        {
+        { 
             sl1++;
             stroka = 0;
         }
