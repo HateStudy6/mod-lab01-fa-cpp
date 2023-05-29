b4fdd0a861b35496ce59b64bb0c2aeddc94a4ce8
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 20:39:28 +0300

    New_Dmitrieva

--- src/fun.cpp
+++ src/fun.cpp
@@ -5,68 +5,69 @@
 
 #include "fun.h"
 
+
 unsigned int faStr1(const char* str)
 
  {
-    int sl1=0; int sl2=0; int stroka=0;
-    for (int j = 0; Mystrlen(str)>i; i++)
-     { if (isalpha(str[i]))
-     {sl1+=1;}
-     if (isdigit(str[i]))
-     {sl2+=1;}
-     if (isspace(str[i]))
-     {if ((sl1>0)&&(sl2==0))
-     {stroka++;}
-     sl1=0;
-     sl2=0;
-     }   
-     }
-    return stroka;
-}
+     int sl1=0; int sl2=0; int stroka=0;  
+     for (int j = 0; Mystrlen(str)>i; i++) 
+     { if (isalpha(str[i])) 
+      {sl1+=1;} 
+     if (isdigit(str[i])) 
+      {sl2+=1;} 
+     if (isspace(str[i])) 
+      {if ((sl1>0)&&(sl2==0)) 
+      {stroka++;} 
+      sl1=0; 
+      sl2=0; 
+     }    
+     } 
+ return stroka; 
+} 
 
 
 
-unsigned int faStr2(const char* str) 
+unsigned int faStr2(const char* str)  
 
-{
-    int sl1=0; int sl2=0; int stroka=0;
-    for (int i = 0; Mystrlen(str)>i; i++ )
-    { if (isdigit(str[i]) || ispunct(str[i]))
-    {sl2+=1;}
-    if (isupper(str[i]) && (sl1=0))
-    {sl1=1;}
-    if (isspace(str[i]))
-    {if ((sl1=1) && (sl2==0))
-    {stroka++;}
-    sl1=0;
-    sl2=0;
-    }
-}
-return stroka;
-}
+ {
+ int sl1=0; int sl2=0; int stroka=0; 
+    for (int i = 0; Mystrlen(str)>i; i++ ) 
+    { if (isdigit(str[i]) || ispunct(str[i])) 
+    { sl2+=1; } 
+    if (isupper(str[i]) && (sl1=0)) 
+    { sl1=1;} 
+    if (isspace(str[i])) 
+    {if ((sl1=1) && (sl2==0)) 
+    {stroka++;} 
+    sl1=0; 
+    sl2=0; 
+    }  
+    }  
+ return stroka; 
+} 
 
 
 unsigned int faStr3(const char* str) 
 
 {
-    int sl1=0; int sl2=0; int stroka=0;
-    float SrDlina=0;
-    for (int i = 0; Mystrlen(str)>i; i++)
-     {
-        if (isalpha(str[i])) 
-        {
-            sl2++;
-            stroka=1;
-        }
-        if (isspace(str[i]) && (stroka==1)) 
-        {
-            sl1++;
-            stroka = 0;
-        }
-    }
-    if (stroka==1)
-    {sl1++;}
-    SrDlina = static_cast<double>(sl2)/sl1;
-    return round(SrDlina);
+    int sl1=0; int sl2=0; int stroka=0;  
+    float SrDlina=0;  
+    for (int i = 0; Mystrlen(str)>i; i++) 
+     { 
+        if (isalpha(str[i]))  
+        { 
+            sl2++; 
+            stroka=1; 
+        } 
+        if (isspace(str[i]) && (stroka==1))  
+        { 
+            sl1++; 
+            stroka = 0; 
+        } 
+    } 
+    if (stroka==1) 
+    {sl1++;} 
+    SrDlina = static_cast<double>(sl2)/sl1; 
+    return round(SrDlina); 
 }
 
