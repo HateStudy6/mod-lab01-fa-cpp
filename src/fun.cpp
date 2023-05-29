a43d8937d9d7ab2da98f5430af7ae7927516d5ed
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 21:55:05 +0300

    ,

--- src/fun.cpp
+++ src/fun.cpp
@@ -44,14 +44,11 @@ unsigned int faStr2(const char* str) {
 unsigned int faStr3(const char* str) { 
     int sl1 = 0; int sl2 = 0; int stroka = 0;
     float SrDlina = 0;
-    for ( int i = 0; strlen(str) > i; i++)
-    {
-    if (isalpha(str[i]))
-    { sl2++;
+    for ( int i = 0; strlen(str) > i; i++) {
+    if (isalpha(str[i])) { sl2++;
     stroka = 1;
     }
-    if (isspace(str[i]) && (stroka == 1))
-    {
+    if (isspace(str[i]) && (stroka == 1)) {
     sl1++;
     stroka = 0;
     }
