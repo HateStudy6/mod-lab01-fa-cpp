2bdbf87cc70651e8bf695f4597286bee20e7b2de
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 21:26:18 +0300

    new6

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,15 +4,15 @@
 #include <cmath>
 
 #include "fun.h"
- 
-unsigned int faStr1(const char* str)
-{
+
+unsigned int faStr1(const char* str) 
+{ 
      int sl1 = 0; int sl2 = 0; int stroka = 0;
-     for (int i = 0; strlen(str) > i; i++)
+     for (int i = 0; strlen(str) > i; i++ )
      { if (isalpha(str[i]))
-      {sl1+= 1;}
+      {sl1 += 1;}
      if (isdigit(str[i]))
-      {sl2+= 1;}
+      {sl2 += 1;}
      if (isspace(str[i]))
       {if ((sl1 > 0)&&(sl2 == 0))
       {stroka++;}
@@ -25,8 +25,8 @@ unsigned int faStr1(const char* str)
 
 
 
-unsigned int faStr2(const char* str)
-{
+unsigned int faStr2(const char* str) 
+{ 
     int sl1 = 0; int sl2 = 0; int stroka = 0;
     for (int i = 0; strlen(str) > i; i++ )
     { if (isdigit(str[i]) || ispunct(str[i]))
@@ -46,16 +46,16 @@ unsigned int faStr2(const char* str)
 
 unsigned int faStr3(const char* str)
 
-{
+{ 
     int sl1 = 0; int sl2 = 0; int stroka = 0;
     float SrDlina = 0;
     for (int i = 0; strlen(str) > i; i++)
-     {
+    { 
         if (isalpha(str[i]))
         { sl2++;
         stroka = 1;
         }
-        if (isspace(str[i]) && (stroka == 1)) 
+        if (isspace(str[i]) && (stroka == 1))
         {
             sl1++;
             stroka = 0;
