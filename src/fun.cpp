0cc00c8e524bce2603253d37cd15a816e90727f6
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-29 21:32:18 +0300

    new7

--- src/fun.cpp
+++ src/fun.cpp
@@ -7,20 +7,20 @@
 
 unsigned int faStr1(const char* str) 
 { 
-     int sl1 = 0; int sl2 = 0; int stroka = 0;
-     for (int i = 0; strlen(str) > i; i++ )
-     { if (isalpha(str[i]))
-      {sl1 += 1;}
-     if (isdigit(str[i]))
-      {sl2 += 1;}
-     if (isspace(str[i]))
-      {if ((sl1 > 0)&&(sl2 == 0))
-      {stroka++;}
-      sl1 = 0;
-      sl2 = 0;
-      }
-      }
-     return stroka;
+  int sl1 = 0; int sl2 = 0; int stroka = 0;
+  for (int i= 0; strlen(str) > i; i++ )
+  { if (isalpha(str[i]))
+  {sl1 + = 1;}
+  if (isdigit(str[i]))
+  {sl2 + = 1;}
+  if (isspace(str[i]))
+  {if ((sl1 > 0)&&(sl2 == 0))
+  {stroka++;}
+  sl1 = 0;
+  sl2 = 0;
+  }
+  }
+  return stroka;
 }
 
 
@@ -51,16 +51,16 @@ unsigned int faStr3(const char* str)
     float SrDlina = 0;
     for (int i = 0; strlen(str) > i; i++)
     { 
-        if (isalpha(str[i]))
-        { sl2++;
-        stroka = 1;
-        }
-        if (isspace(str[i]) && (stroka == 1))
-        {
-            sl1++;
-            stroka = 0;
-        }
+    if (isalpha(str[i]))
+    { sl2++;
+    stroka = 1;
     }
+    if (isspace(str[i]) && (stroka == 1))
+    {
+    sl1++;
+    stroka = 0;
+    }
+}
     if (stroka == 1)
     {sl1++;}
     SrDlina = static_cast<double>(sl2)/sl1;
